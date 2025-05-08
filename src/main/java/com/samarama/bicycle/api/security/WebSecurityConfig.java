8edeb8c86ad57eef2de13331fd065672a6d4c5ec
mimik91 <dominiklach@poczta.fm>
2025-05-08 17:16:59 +0200

    change paths to heroku

--- src/main/java/com/samarama/bicycle/api/security/WebSecurityConfig.java
+++ src/main/java/com/samarama/bicycle/api/security/WebSecurityConfig.java
@@ -78,7 +78,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("https://samarama-front-7d7f603b041c.herokuapp.com/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
